17c45728610d14bb3b12455208896004012cceee
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-06-13 19:49:08 +0200

    feat(front): ajout la barre de navigation

--- angular-client/src/app/shared/shared.module.ts
+++ angular-client/src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavComponent } from './components/nav/nav.component';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavComponent} from './components/nav/nav.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
   declarations: [
     NavComponent
   ],
+  exports: [
+    NavComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
