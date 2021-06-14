251976ff03f40024f9d095f83d4254ad484f52d4
matthieuaudemard <matthieu.audemard@gmail.com>
2021-06-14 19:04:55 +0200

    feat(auth): ajoute le composant login (#8)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
