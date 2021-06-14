d44baeaaf56296809262745cf78ee5847b2f389d
matthieuaudemard <matthieu.audemard@gmail.com>
2021-06-14 19:51:18 +0200

    feat(api): ajoute la récupération des jeux en base de données (#10)
     * ajout des dépendances pymong et pyyaml
     * ajout de la connexion à la base de données

--- app.py
+++ app.py
@@ -1,44 +1,30 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import yaml
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+config = yaml.full_load(open('database.yaml'))
+client = MongoClient(config['uri'])
+db = client['azure']
+
 
 @app.route("/api/games")
 def get_():
-    return jsonify([{
-        'name': 'Pinball Clash',
-        'cover': 'https://images.crazygames.com/pinball-clash/20210611080612/pinball-clash-cover?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-        'review': 4
-    }, {
-        'name': 'pac-xson',
-        'cover': 'https://images.crazygames.com/games/pac-xon/cover-1586285413312.jpg?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-        'review': 3
-    }, {
-        'name': 'Injustice Gods Among Us',
-        'cover': 'https://images.crazygames.com/injusticegodsamongus.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-    }, {
-        'name': 'Crused Treasure',
-        'cover': 'https://images.crazygames.com/games/cursed-treasure/cover-1588758575748.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-        'review': 3
-    }, {
-        'name': 'BombIt 6',
-        'cover': 'https://images.crazygames.com/bombit6.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-        'review': 3
-    }, {
-        'name': 'Bomb Ball 3D',
-        'cover': 'https://images.crazygames.com/bomb-balls-3d-cover?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-        'review': 3
-    }, {
-        'name': 'Gold Digger',
-        'cover': 'https://images.crazygames.com/games/gold-digger-frvr/cover-1608107647188.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-        'review': 3
-    }, {
-        'name': 'Fireboy & Watergirl',
-        'cover': 'https://images.crazygames.com/games/fireboy-and-watergirl-5-elements/cover-1586285304860.jpg?auto=format,compress&q=75&cs=strip&ch=DPR&w=178&h=100&fit=crop',
-        'review': 3
-    }])
+    all_data = db['games'].find()
+    data_json = []
+    for data in all_data:
+        data_json.append({
+            '_id': str(data['_id']),
+            'name': data['name'],
+            'cover': data['cover'],
+            'review': str(data['review']) if 'review' in data else '0'
+        })
+    print(data_json)
+    return jsonify(data_json)
 
 
 app.run()
