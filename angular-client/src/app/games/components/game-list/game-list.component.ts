657f4ecf09b3b12bd30119ee4b068ebbc656e0b2
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-06-30 01:37:57 +0200

    feat: ajoute les toasters pour le démarrage et l'arrêt de la VM

--- angular-client/src/app/games/components/game-list/game-list.component.ts
+++ angular-client/src/app/games/components/game-list/game-list.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {GameService} from '../../services/game.service';
 import {Game} from '../../models/game';
-import {AuthService} from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-game-list',
